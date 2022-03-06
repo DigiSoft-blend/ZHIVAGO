3549f8d7f761dadfedc68d5ea9577eb3ebbd12b7
unknown <silasudofia469@gmail.com>
2022-03-06 12:51:34 -0800

    upgrade

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,11 +2,13 @@
 import TheWelcome from '@/components/TheWelcome.vue'
 import Navbar from '../components/Navbar.vue';
 import SideNav from '../components/SideNav.vue';
+import LoginView from './LoginView.vue';
 </script>
 
 <template>
-    <div class="container-scroller" style="background-color:whitesmoke">
+ <LoginView></LoginView>
+    <!-- <div class="container-scroller" style="background-color:whitesmoke">
        <SideNav></SideNav>
         <Navbar></Navbar>
-     </div>
+     </div> -->
 </template>
