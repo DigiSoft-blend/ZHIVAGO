eb5a238b1c3c8ec322ebd503d9bc53faa0bf2db0
unknown <silasudofia469@gmail.com>
2022-03-06 12:33:14 -0800

    upgrade

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,8 +1,8 @@
 <template>
 <nav class="sidebar sidebar-offcanvas" id="sidebar">
         <div class="sidebar-brand-wrapper d-none d-lg-flex align-items-center justify-content-center fixed-top  border-bottom  border-secondary">
-          <a class="sidebar-brand brand-logo" href="index.html"><img src="assets/images/zhivago-logo .png" alt="logo" /></a>
-          <a class="sidebar-brand brand-logo-mini" href="index.html"><img src="assets/images/logo-mini.svg" alt="logo" /></a>
+          <a class="sidebar-brand brand-logo" href="index.html"><img src="/assets/images/zhivago-logo.png" alt="logo" /></a>
+          <a class="sidebar-brand brand-logo-mini" href="index.html"><img src="/assets/images/logo-mini.svg" alt="logo" /></a>
         </div>
         <ul class="nav">
           <!-- <li class="nav-item profile">
